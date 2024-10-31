eedd5926959e1021f81af0514a1486fc056a3566
EtienneTessier <etienne.tessier1@gmail.com>
2024-10-31 15:07:54 +0100

    opti fonction shoot step/2

--- sources/action/shoot.c
+++ sources/action/shoot.c
@@ -18,32 +18,24 @@
 
 static int	is_on_enemy(int shoot_x, int shoot_y, t_enemy enemy)
 {
-	ft_printf("test\n");
-	printf("enemy.x = %f, enemy.y = %f\n", enemy.x, enemy.y);
-	printf("shoot_x = %d, shoot_y = %d\n", shoot_x, shoot_y);
 	if ((shoot_x == (int)enemy.x) && (shoot_y == (int)enemy.y))
-		return (printf("touche\n"), 1);
+		return (1);
 	return (0);
 }
 
 int	shoot(t_player player, t_data *data)
 {
 	int		i;
-	int		shoot_x;
-	int		shoot_y;
+	double	shoot_x;
+	double	shoot_y;
 	double	distance;
 
-	ft_printf("shoot\n");
 	i = -1;
 	while (++i < data->ennemis_count)
 	{
 		shoot_x = player.x;
 		shoot_y = player.y;
 		distance = 0;
-		printf("distance = %f\n", distance);
-		printf("data->ennemis[%d].distance = %f\n", i, data->ennemis[i].distance);
-		ft_printf("shoot_x = %d\n", shoot_x);
-		ft_printf("shoot_y = %d\n", shoot_y);
 		while (distance <= data->ennemis[i].distance && data->ennemis[i].alive)
 		{
 			if (is_on_enemy(shoot_x, shoot_y, data->ennemis[i]))
@@ -51,8 +43,8 @@ int	shoot(t_player player, t_data *data)
 				data->ennemis[i].alive = 0;
 				return (1);
 			}
-			shoot_x += player.dir_x;
-			shoot_y += player.dir_y;
+			shoot_x += player.dir_x / 2;
+			shoot_y += player.dir_y / 2;
 			distance = ((player.x - shoot_x) * (player.x - shoot_x) + \
 				(player.y - shoot_y) * (player.y - shoot_y));
 		}
