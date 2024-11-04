de0074c69af8bf674796a701d6fa35c8234db63e
Etienne Tessier <etessier@made-f0Br6s5.clusters.42paris.fr>
2024-11-04 14:05:05 +0100

    norm ok ajout texture porte hi

--- sources/init/init_data.c
+++ sources/init/init_data.c
@@ -12,22 +12,6 @@
 
 #include "../includes/cub3d.h"
 
-// static t_data	*crea_data(void)
-// {
-// 	t_data	*data;
-
-// 	data = ft_calloc(1, sizeof(t_data));
-// 	if (!data)
-// 		exit(1);
-// 	data->mlx = NULL;
-// 	data->win = NULL;
-// 	data->img = NULL;
-// 	data->map = NULL;
-// 	data->txr = NULL;
-// 	data->ennemis_count = 0;
-// 	return (data);
-// }
-
 static t_txr	*crea_txr(t_data *data)
 {
 	t_txr	*txr;
@@ -54,10 +38,10 @@ static t_map	*crea_map(t_data *data)
 	map = ft_calloc(1, sizeof(t_map));
 	if (!map)
 		exit_pgm(data, 1);
+	map->fd = -1;
 	map->map2d = NULL;
 	map->map2d_copy = NULL;
 	map->minimap = NULL;
-	map->fd = -1;
 	return (map);
 }
 
@@ -72,31 +56,6 @@ static t_img	*crea_img(t_data *data)
 	img->img = NULL;
 	return (img);
 }
-
-// t_data	*init_data(char *map_path)
-// {
-// 	t_data	*data;
-
-// 	data = crea_data();
-// 	data->map = crea_map(data);
-// 	data->img = crea_img(data);
-// 	data->mlx = mlx_init(data);
-// 	if (!data->mlx)
-// 		return (free_data(data), NULL);
-// 	data->txr = crea_txr(data);
-// 	if (init_textures(map_path, data))
-// 		return (free_data(data), NULL);
-// 	if (init_map(data))
-// 		return (free_data(data), NULL);
-// 	if (BONUS)
-// 		if (init_bonus(data))
-// 			return (free_data(data), NULL);
-// 	data->img->img = mlx_new_image(data->mlx, SCR_WIDTH, SCR_HEIGHT);
-// 	data->img->addr = (int *)mlx_get_data_addr(data->img->img, &data->img->bpp, \
-// 		&data->img->line_length, &data->img->endian);
-// 	data->win = mlx_new_window(data->mlx, SCR_WIDTH, SCR_HEIGHT, "cub3d");
-// 	return (data);
-// }
 
 int	init_data(t_data *data, char *map_path)
 {
